eb217a7b10d7e2f79e8da820d150d3c6041b2f5b
Elijah Ngocho <elijahngocho24@gmail.com>
2023-11-06 22:48:52 +0300

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -5,9 +5,29 @@ import Home from "./Home";
 import Actors from "./Actors";
 import Directors from "./Directors";
 import Movies from "./Movies";
+import { movies, actors, directors } from "../data"; // Import your data
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <NavBar />
+      <Switch>
+        <Route
+          path="/movies"
+          render={() => <Movies movies={movies} />}
+        />
+        <Route
+          path="/directors"
+          render={() => <Directors directors={directors} />}
+        />
+        <Route
+          path="/actors"
+          render={() => <Actors actors={actors} />}
+        />
+        <Route path="/" exact component={Home} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
